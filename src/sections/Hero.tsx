22cd159a69e3f9ede7b2060856e994bb46c36a0b
Harshalvk <harshalvkhobragade@gmail.com>
2024-11-25 22:39:36 +0530

    ADD: Complete Hero animations

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,25 +1,98 @@
+"use client";
 import Button from "@/components/Button";
 import designExample1Image from "@/assets/images/design-example-1.png";
 import designExample2Image from "@/assets/images/design-example-2.png";
 import Image from "next/image";
 import Pointer from "@/components/Pointer";
+import { motion, useAnimate } from "framer-motion";
+import { useEffect } from "react";
+import cursorYouImage from "@/assets/images/cursor-you.svg";
 
 export default function Hero() {
+  const [leftDesignScope, leftDesignAnimate] = useAnimate();
+  const [leftPointerScope, leftPointerAnimate] = useAnimate();
+  const [rightDesignScope, rightDesignAnimate] = useAnimate();
+  const [rightPointerScope, rightPointerAnimate] = useAnimate();
+
+  useEffect(() => {
+    leftDesignAnimate([
+      [leftDesignScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [leftDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+    ]);
+
+    leftPointerAnimate([
+      [leftPointerScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [leftPointerScope.current, { y: 0, x: -100 }, { duration: 0.5 }],
+      [
+        leftPointerScope.current,
+        { x: 0, y: [0, 16, 0] },
+        { duration: 0.5, ease: "easeInOut" },
+      ],
+    ]);
+
+    rightDesignAnimate([
+      [rightDesignScope.current, { opacity: 1 }, { duration: 0.5, delay: 1.5 }],
+      [rightDesignScope.current, { x: 0, y: 0 }, { duration: 0.5 }],
+    ]);
+
+    rightPointerAnimate([
+      [
+        rightPointerScope.current,
+        { opacity: 1 },
+        { duration: 0.5, delay: 1.5 },
+      ],
+      [rightPointerScope.current, { x: 175, y: 0 }, { duration: 0.5 }],
+      [rightPointerScope.current, { x: 0, y: [0, 20, 0] }, { duration: 0.5 }],
+    ]);
+  }, []);
+
   return (
-    <section className="py-24 overflow-x-clip">
+    <section
+      className="py-24 overflow-x-clip"
+      style={{
+        cursor: `url(${cursorYouImage.src}), auto`,
+      }}
+    >
       <div className="container relative">
-        <div className="hidden lg:block absolute -left-36 top-16">
-          <Image src={designExample1Image} alt="desing example 1 image" />
-        </div>
-        <div className="hidden lg:block absolute -right-[272px] -top-16">
-          <Image src={designExample2Image} alt="desing example 2 image" />
-        </div>
-        <div className="hidden lg:block absolute left-56 top-96">
+        <motion.div
+          ref={leftDesignScope}
+          initial={{ opacity: 0, y: 100, x: -100 }}
+          drag
+          className="hidden lg:block absolute -left-36 top-16"
+        >
+          <Image
+            src={designExample1Image}
+            alt="desing example 1 image"
+            draggable="false"
+          />
+        </motion.div>
+        <motion.div
+          ref={leftPointerScope}
+          initial={{ opacity: 0, y: 100, x: -200 }}
+          className="hidden lg:block absolute left-56 top-96"
+        >
           <Pointer name="Andres" />
-        </div>
-        <div className="hidden lg:block absolute right-80 -top-4">
+        </motion.div>
+
+        <motion.div
+          ref={rightDesignScope}
+          initial={{ opacity: 0, x: 100, y: 100 }}
+          drag
+          className="hidden lg:block absolute -right-[272px] -top-16"
+        >
+          <Image
+            src={designExample2Image}
+            alt="desing example 2 image"
+            draggable="false"
+          />
+        </motion.div>
+        <motion.div
+          ref={rightPointerScope}
+          initial={{ opacity: 0, x: 275, y: 100 }}
+          className="hidden lg:block absolute right-80 -top-4"
+        >
           <Pointer name="Bryan" color="red" />
-        </div>
+        </motion.div>
 
         <div className="flex justify-center">
           <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
