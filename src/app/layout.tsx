ff05a30f47b28967a16e40f2839d1fdd79b2cee8
Harshalvk <harshalvkhobragade@gmail.com>
2024-12-13 17:30:09 +0530

    FIX: changes for seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-    title: "Modern Design Tool Landing Page",
-    description: "Created with the help of Frontend Tribe",
+    title: "Layers",
+    description: "A desing agency landing page.",
 };
 
 export default function RootLayout({
