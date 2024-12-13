335feebeec8f774408e9ac56e25c7b6f9606db81
Harshalvk <harshalvkhobragade@gmail.com>
2024-12-13 17:14:31 +0530

    FIX: optimaizations for deployment

--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Tag from "@/components/Tag";
-import { useMotionValueEvent, useScroll, useTransform } from "framer-motion";
-import { useEffect, useInsertionEffect, useRef, useState } from "react";
+import { useScroll, useTransform } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 const text = `You're racing to create exceptional work, but traditional design tools slow you down with unnecessary complexity and steep learning curves.`;
